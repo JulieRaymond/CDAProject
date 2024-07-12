6d958625daeb200f380a8be6aeeb1605f9ced217
Julie Raymond <julie.raymond@pichet.com>
2024-07-12 15:52:05 +0200

    Added : ngx-dropzone-wrapper to handle drop zone area for image

--- a2-runalliance-front/src/app/app.component.ts
+++ a2-runalliance-front/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {MatIconModule} from "@angular/material/icon";
 import {HomepageComponent} from "./features/homepage/homepage.component";
 import {MAT_DATE_LOCALE} from "@angular/material/core";
@@ -7,15 +7,27 @@ import {NgOptimizedImage} from "@angular/common";
 import {ButtonComponent} from "./components/button/button.component";
 import {NavigationBarComponent} from "./components/navigation-bar/navigation-bar.component";
 import {FooterComponent} from "./components/footer/footer.component";
+import {DROPZONE_CONFIG, DropzoneConfigInterface, DropzoneModule} from "ngx-dropzone-wrapper";
+
+const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
+  url: 'https://httpbin.org/post', // URL où envoyer les fichiers uploadés
+  maxFiles: 1, // Limite le nombre de fichiers à 1
+  maxFilesize: 50, // Taille maximale des fichiers en Mo
+  acceptedFiles: 'image/*' // Types de fichiers acceptés (ici seulement des images)
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, MatIconModule, NavigationBarComponent, FooterComponent, HomepageComponent, NgOptimizedImage],
+  imports: [RouterOutlet, ButtonComponent, MatIconModule, NavigationBarComponent, FooterComponent, HomepageComponent, NgOptimizedImage, DropzoneModule],
   providers: [
     {provide: MAT_DATE_LOCALE, useValue: 'fr-FR'},
+    {
+      provide: DROPZONE_CONFIG,
+      useValue: DEFAULT_DROPZONE_CONFIG
+    }
   ],
-    templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
--- a2-runalliance-front/src/app/components/edit-profile/edit-profile.component.ts
+++ a2-runalliance-front/src/app/components/edit-profile/edit-profile.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {DropzoneConfigInterface, DropzoneModule} from "ngx-dropzone-wrapper";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @Component({
   selector: 'app-edit-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSlideToggle,
+    DropzoneModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.scss'
 })
 export class EditProfileComponent {
+  public profileForm: FormGroup;
+  public config: DropzoneConfigInterface = {
+    url: 'https://httpbin.org/post',
+    maxFilesize: 50,
+    acceptedFiles: 'image/*'
+  };
 
+  constructor(private fb: FormBuilder) {
+    this.profileForm = this.fb.group({
+      name: [''],
+      email: [''],
+      isVisible: [false] // Contrôle pour la visibilité du profil, initialisé à false (non visible)
+    });
+  }
+
+  onFileUpload(event: any): void {
+    console.log('File uploaded:', event);
+    // Ajoutez ici la logique de traitement du téléchargement du fichier si nécessaire
+  }
+
+  onSubmit(): void {
+    console.log('Form submitted:', this.profileForm.value);
+    // Ajoutez ici la logique de sauvegarde du formulaire
+  }
 }
